c0464c610edb1245ce7373a7afe3961fd76d7b59
zhangyi921 <yi@vividtheory.com>
2022-02-09 14:22:08 -0700

    a2 task 2

--- src/A2/task1.ts
+++ src/A2/task1.ts
@@ -1,45 +1,33 @@
 // a. f: A -> [A] -> [A]
 const concat = <A>(a: A, arr: A[]): A[] => arr.concat(a)
 const concat2 = <A>(a: A): (arr: A[]) => A[] => {
-    // implementation
     return (arr) => arr.concat(a)
 }
 
 // b. f: Number -> Number -> [A] -> [A]
-const apply = <A>(num: number, f: (num: number) => A[]): A[] => f(num)
-const swap = <A>(index1: number, index2: number): (arr: A[]) => A[] => {
-    // implementation
-    return (arr) => {
-        const tempA: A = arr[index1]
-        arr[index1] = arr[index2]
-        arr[index2] = tempA
-        return arr
-    }
-}
-const swap2 = <A>(index1: number, index2: number, arr: A[]): A[] => {
-    // implementation
-    const tempA: A = arr[index1]
-    arr[index1] = arr[index2]
-    arr[index2] = tempA
-    return arr
+const applyTo = <A>(num: number, f: (num: number) => A[]): A[] => f(num)
+
+const slice = <A>(start: number, end: number, arr: A[]): A[] => arr.slice(start, end)
+const slice2 = <A>(start: number, end: number): (arr: A[]) => A[] => {
+    return (arr) => arr.slice(start, end)
 }
 
 // c. f: [String] -> {String: any} -> {String: any}
-const deleteProperty = (str: string, obj: Record<string, any>): Record<string, any> => {
-    delete obj[str]
-    return obj
+const extractProperties = (keys: string[], obj: Record<string, any>): Record<string, any> => {
+    const result: Record<string, any> = {}
+    keys.forEach(key => result[key] = obj[key])
+    return result
 }
-const deleteProperty2 = (str: string): (obj: Record<string, any>) => Record<string, any> => {
-    // implementation
+const extractProperties2 = (keys: string[]): (obj: Record<string, any>) => Record<string, any> => {
     return (obj) => {
-        delete obj[str]
-        return obj
+        const result: Record<string, any> = {}
+        keys.forEach(key => result[key] = obj[key])
+        return result
     }
 }
 
 // d. f: [A] → [B] → [[A,B]]
 const createPair = <A, B>(a: A[], b: B[]): [A, B][] => a.map((a, i) => [a, b[i]])
 const createPair2 = <A, B>(a: A[]): (b: B[]) => [A, B][] => {
-    // implementation
     return (b) => a.map((a, i) => [a, b[i]])
 }
--- src/A2/task2.ts
+++ src/A2/task2.ts
@@ -1,3 +1,4 @@
+import * as R from 'ramda'
 interface Entity {
     id: string;
 }
@@ -21,35 +22,114 @@ type ApiResponse<T extends Entity> =
     | { status: "success"; data: T[] }
     | { status: "error"; error: string };
 
+/* 1 */
+function fetchMockData(): Promise<ApiResponse<Post>>;
+function fetchMockData(postId: number): Promise<ApiResponse<Comment>>;
+function fetchMockData(postId?:number): Promise<ApiResponse<Post | Comment>> {
 
-// type fetchPosts = (type: 'posts') => Promise<Post[]>
-// type fetchComments = (type: 'comments') => Promise<Comment[]>
-// const fetchMockData: fetchPosts | fetchComments = async (type) => {
-//     if (type === )
-// }
-// const fetchMockData = async <T, K>(type) => {
-//     if (type === )
-// }
-
-function fetchMockData(getPosts: 'posts'): Promise<Post[]>;
-function fetchMockData(getComments: 'comments'): Promise<Comment[]>;
-function fetchMockData(type) {
-
-  if (type === 'posts'){
-    return new Promise(resolve => {
-        setTimeout(() => {
-          resolve([] as Comment[]);
-        }, 2000);
-      });
-  }else {
-    return new Promise(resolve => {
-        setTimeout(() => {
-          resolve('resolved');
-        }, 2000);
-      });
-  }
+    if (postId === undefined) {
+        return new Promise(resolve => {
+            fetch('https://jsonplaceholder.typicode.com/posts/1')
+                .then((response) => response.json())
+                .then((json) => resolve({status: 'success', data: json as Post[]}))
+                .catch(err => resolve({status: 'error', error: JSON.stringify(err)}));
+        });
+    } else {
+        return new Promise(resolve => {
+            fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+                .then((response) => response.json())
+                .then((json) => resolve({status: 'success', data: json as Comment[]}))
+                .catch(err => resolve({status: 'error', error: JSON.stringify(err)}));
+        });
+    }
 }
 
-const f = async () => {
-    const reuslt = await fetchMockData('posts')
+/* 2 */
+const fetchComments = R.partial(fetchMockData, ['comments'])
+const fetchPosts = fetchMockData
+
+async () => {
+    const commentsRemoteData: ApiResponse<Comment> = await fetchComments(6)
+    const postsRemoteData: ApiResponse<Post> = await fetchPosts()
 }
+
+/* 3 */
+type Matcher <D> = {
+    success: (data: D) => void;
+    error: (error: string) => void
+}
+const match = <T extends Entity>(rd: ApiResponse<T>, matcher: Matcher<T[]>) => {
+    if (rd.status === 'success'){
+        matcher[rd.status](rd.data)
+        return rd.data
+    }else {
+        matcher[rd.status](rd.error)
+        return rd.error
+    }
+}
+
+/* 4 */
+async () => {
+    const postsRemoteData: ApiResponse<Post> = await fetchPosts()
+    match(postsRemoteData, {
+        error: (err) => console.log('Error is', err),
+        success: async (posts) => {
+            console.log('All posts:', posts)
+            if (posts.length > 0){
+                // log first post's comments
+                const commentsRemoteData: ApiResponse<Comment> = await fetchComments(posts[0].id) 
+                match(commentsRemoteData, {
+                    error: (err) => console.log('Error is', err),
+                    success: (comments) => console.log(`Post ${posts[0].id}'s comments: `, comments)
+                })
+            }
+        }
+    })
+}
+
+/* 5 */
+const application = async () => {
+    const postsRemoteData: ApiResponse<Post> = await fetchPosts()
+    match(postsRemoteData, {
+        error: (err) => console.log('Error is', err),
+        success: async (posts) => {
+            // get user 1's posts comments count
+            const user1posts = posts.filter(post => post.userId)
+            const count = await user1posts.reduce(async (count, currentPost) => {
+                const commentsRemoteData: ApiResponse<Comment> = await fetchComments(currentPost.id)
+                const comments = match(commentsRemoteData, {
+                    error: (err) => console.log('Error is', err),
+                    success: (comments) => comments
+                })
+                if (Array.isArray(comments)){
+                    return (await count) + comments.length
+                }else {
+                    return count
+                }
+            }, Promise.resolve(0))
+        }
+    })
+}
+
+/**
+ * 1. We can use Either object to wrap the result. The result can be data or error message.
+ * 
+ * 2. This program can be written in a React application. The application can load posts/comments as user click/gavigate on pages. 
+ * 
+ * 3. The match function is pure. fetchMockData, fetchComments, fetchPosts, and application is impure
+ * 
+ * 4. RemoteData ADT
+ * 
+ */
+
+type Success<T> = {
+    tag: "success",
+    data: T
+}
+
+type Error<E> = {
+    tag: "error"
+    error: E
+}
+
+type RemoteData<D, E> = Success<D> | Error<E>
