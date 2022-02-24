c97ca7042541a9c37f5ed6cbe6c649ed12190f5c
zhangyi921 <yi@vividtheory.com>
2022-02-24 12:34:15 -0700

    task 3

--- src/A4/task2.ts
+++ src/A4/task2.ts
@@ -1,11 +1,31 @@
 type FormError<FormDef> = {
     [K in keyof FormDef]?: {
         errorMsg: string;
-        resolveHint: string
     }
 }
 
 interface Form<T> {
     formValues: T;
     formErrors: FormError<T>
+}
+
+// example
+
+interface UserRegistrationDto {
+    name: string;
+    age: number;
+    password: string;
+    confirmPassword: string
+}
+
+const validationResult: Form<UserRegistrationDto> = {
+    formValues: {name: '', age: 26, password: '@', confirmPassword: '#'},
+    formErrors: {
+        name: {
+            errorMsg: 'Name cannot be empty'
+        },
+        confirmPassword: {
+            errorMsg: 'Password does not match'
+        }
+    }
 }
--- src/A4/task3.ts
+++ src/A4/task3.ts
@@ -1,4 +1,5 @@
 type Tree<A> = Leaf<A> | Branch<A>;
+type ZippedTree<A> = ZippedLeaf<A> | ZippedBranch<A>
 
 class Leaf<A> {
     tag: "leaf" = "leaf";
@@ -9,77 +10,125 @@ class Leaf<A> {
     }
 }
 
-class ZippedLeaf<A> {
-    tag: "leaf" = "leaf";
-    readonly values: A[];
-
-    constructor(value: A, value2: A) {
-        this.values = [value, value2];
-    }
-}
-
+// made left and right optional for easier filtering
 class Branch<A> {
     tag: "branch" = "branch";
-    readonly left: Tree<A>;
-    readonly right: Tree<A>;
+    readonly left?: Tree<A>;
+    readonly right?: Tree<A>;
 
-    constructor(left: Tree<A>, right: Tree<A>) {
-        this.left = left;
-        this.right = right;
+    constructor(leaves: { left?: Tree<A>, right?: Tree<A> }) {
+        this.left = leaves.left;
+        this.right = leaves.right;
     }
 }
 
+class ZippedLeaf<A> {
+    tag: "leaf" = "leaf";
+    readonly values: A[] = [];
+
+    constructor(leaves: A[]) {
+        this.values = leaves;
+    }
+}
+
+class ZippedBranch<A> {
+    tag: "branch" = "branch";
+    readonly left?: ZippedTree<A>;
+    readonly right?: ZippedTree<A>;
+    readonly value?: A; // for zip a leaf with a branch
+
+    constructor(leaves: { left?: ZippedTree<A>, right?: ZippedTree<A>, value?: A }) {
+        this.left = leaves.left;
+        this.right = leaves.right;
+        this.value = leaves.value
+    }
+}
+
+// type gruard
 const isLeaf = <A>(tree: Tree<A>): tree is Leaf<A> => tree.tag === 'leaf'
 const isBranch = <A>(tree: Tree<A>): tree is Branch<A> => tree.tag === 'branch'
 
 // 1.
-const size = <A>(tree: Tree<A>): { leaves: number, branch: number } => {
+type SizeResult = { leaves: number, branch: number }
+const size = <A>(tree: Tree<A>): SizeResult => {
     if (isBranch(tree)) {
+        const leftSize: SizeResult = tree.left ? size(tree.left) : { leaves: 0, branch: 0 }
+        const rightSize: SizeResult = tree.right ? size(tree.right) : { leaves: 0, branch: 0 }
         return {
-            leaves: size(tree.left).leaves + size(tree.right).leaves,
-            branch: 1 + size(tree.left).branch + size(tree.right).branch
+            leaves: leftSize.leaves + rightSize.leaves,
+            branch: leftSize.branch + rightSize.branch + 1
         }
     }
-    return { leaves: 1, branch: 0 }
+    return { leaves: 1, branch: 0 } // this is a leaf
 }
 
-// 2.
-const max = <A>(tree: Tree<A>): A => {
-    if (isBranch(tree)) {
-        const leftMax = max(tree.left)
-        const rightMax = max(tree.right)
-        return leftMax > rightMax ? leftMax : rightMax
+// 2. undefined is returned when this tree has no leaf
+const max = <A>(tree?: Tree<A>): A | undefined => {
+    if (tree) {
+        if (isBranch(tree)) {
+            const leftMax = max(tree.left)
+            const rightMax = max(tree.right)
+            if (leftMax && rightMax) return leftMax > rightMax ? leftMax : rightMax
+            return leftMax ? leftMax : rightMax ? rightMax : undefined // possibly return undefined here
+        } else {
+            return tree.value // this is a leaf
+        }
     }
-    return tree.value
 }
 
 // 3.
 const depth = <A>(tree: Tree<A>): number => {
     if (isBranch(tree)) {
-        const leftDepth = depth(tree.left) + 1
-        const rightDepth = depth(tree.right) + 1
+        const leftDepth = tree.left ? depth(tree.left) + 1 : 0
+        const rightDepth = tree.right ? depth(tree.right) + 1 : 0
         return leftDepth > rightDepth ? leftDepth : rightDepth
     }
-    return 1
+    return 0 // this is a leaf
 }
 
 // 4.
-const mapTree = <A, B>(f: (a: A) => B, tree: Tree<A>): Tree<B> => {
-    if (isBranch(tree)) {
-        return new Branch(mapTree(f, tree.left), mapTree(f, tree.right))
+const mapTree = <A, B>(f: (a: A) => B, tree: Tree<A>): Tree<B> => isBranch(tree) ?
+    new Branch({
+        left: tree.left ? mapTree(f, tree.left) : undefined,
+        right: tree.right ? mapTree(f, tree.right) : undefined
+    }) :
+    new Leaf(f(tree.value))
+
+// 5. Remove node(leaf) if condition is true
+const filter = <A>(f: (a: A) => boolean, tree: Branch<A>): Branch<A> => new Branch({
+    left: tree.left && isLeaf(tree.left) ? (f(tree.left.value) ? undefined : tree.left) : tree.left,
+    right: tree.right && isLeaf(tree.right) ? (f(tree.right.value) ? undefined : tree.right) : tree.right
+})
+
+// 6. branch + branch = branch, leaf + leaf = leaf, branch + leaf = branch
+const zip = <A>(tree1: Tree<A>, tree2?: Tree<A>): ZippedTree<A> => {
+    if (!tree2) { // only tree1
+        if (isLeaf(tree1)) {
+            return new ZippedLeaf([tree1.value])
+        } else {
+            return new ZippedBranch({
+                left: tree1.left ? zip(tree1.left) : undefined,
+                right: tree1.right ? zip(tree1.right) : undefined
+            })
+        }
+    } else { // zip tree1 an tree2
+        if (isLeaf(tree1)) { // tree 1 is leaf
+            if (isLeaf(tree2)) {
+                return new ZippedLeaf([tree1.value, tree2.value])
+            } else {
+                return new ZippedBranch({
+                    left: tree2.left ? zip(tree2.left) : undefined,
+                    right: tree2.right ? zip(tree2.right) : undefined,
+                    value: tree1.value
+                })
+            }
+        } else { // tree 1 is branch
+            return new ZippedBranch({
+                left: tree1.left ? zip(tree1.left, isLeaf(tree2) ? tree2 : tree2.left) : undefined,
+                right: tree1.right ? zip(tree1.right, isLeaf(tree2) ? tree2 : tree2.right) : undefined,
+                value: isLeaf(tree2) ? tree2.value : undefined
+            })
+        }
     }
-    return new Leaf(f(tree.value))
-}
 
-// 5. Remove node if condition is true
-const filter = <A> (f: (a: A) => boolean, tree: Tree<A>): Tree<A> => {
-    if (isBranch(tree)){
-        return new Branch(filter(f, tree.left), filter(f, tree.right))
-    }
-
-}
-
-// 6. 
-const zip = <A> (tree1: Tree<A>, tree2: Tree<A>): Tree<[A, A]> => {
-    
 }
